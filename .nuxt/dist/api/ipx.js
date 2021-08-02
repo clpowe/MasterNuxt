713bd25aedfd26c0fabcd908de1a604f196f68d8
clpowe <clpowe@gmail.com>
2021-08-02 12:37:27 -0400

    fix

--- .nuxt/dist/api/ipx.js
+++
@@ -1,3 +0,0 @@
-import {createIPX, createIPXMiddleware} from "ipx";
-const ipx = createIPX({"dir":"/Volumes/chrisSSD/Coding-practice/NUXTBNB/static","domains":[],"sharp":{},"alias":{}});
-export default createIPXMiddleware(ipx);
