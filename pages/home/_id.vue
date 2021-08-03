82ec4d18d12ce29cb41ceb9ead81de1a5aa2c919
clpowe <clpowe@gmail.com>
2021-08-03 09:15:40 -0400

    Update _id.vue

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -40,6 +40,11 @@ export default {
           hid: "og-url",
           property: "og:url",
           content: `${this.$config.rootUrl}/home/${this.home.objectID}`
+        },
+        {
+          hid: "t-type",
+          name: "twitter:card",
+          content: `summary_Large_image`
         }
       ]
     };
